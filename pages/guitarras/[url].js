035a115d8550dc63225cfd59ab42f5b8ae42a6b3
Lautaro <fayanaslautaroagustin@gmail.com>
2022-11-23 17:31:23 -0300

    ProyectoFinalizado

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,13 +1,39 @@
 //Se debe crear un archivo entre corchetes para crear un Routing Dinamico en NextJS
 
+import { useState } from "react"
 import Image from "next/image"
 import Layout from "../../components/layout"
 import styles from '../../styles/guitarras.module.css'
 
 
-export default function Producto({ guitarra }) {
+export default function Producto({ guitarra , agregarCarrito }) {
 
+    const [ cantidad , setCantidad ] = useState(0)
+    console.log(cantidad);
     const { nombre, descripcion, imagen, precio } = guitarra[0].attributes
+
+    const handleSubmit = ( e ) => {
+        e.preventDefault();
+        
+        if(cantidad < 1){
+            alert('Cantidad no valida');
+            return
+        }
+
+        //Construir un objeto para el storage
+
+        const guitarraSeleccionada = {
+            id: guitarra[0].id,
+            imagen: imagen.data[0].attributes.url,
+            nombre,
+            precio,
+            cantidad
+        }
+
+        //Pandanso la info al context
+        agregarCarrito(guitarraSeleccionada)
+
+    }
 
     return (
         <Layout
@@ -22,6 +48,25 @@ export default function Producto({ guitarra }) {
                     <h3> {nombre} </h3>
                     <p className={styles.descripcion}> {descripcion} </p>
                     <p className={styles.precio}> ${precio} </p>
+
+                    <form
+                    onSubmit={handleSubmit}
+                    className={styles.formulario}>
+                        <label htmlFor="cantidad">Cantidad:</label>
+
+                        <select 
+                            onChange={ e => setCantidad(e.target.value)}
+                            id='cantidad'>
+                                <option value="0">--Seleccione--</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                        </select>
+
+                            <input type='submit' value='Agregar al carrito' />
+                    </form>
 
                 </div>
 
